01057a71d478391bc101d79b34651c7c8803ff36
Ramya Krishnan <ramyaneela36@gmail.com>
2023-08-05 14:52:17 -0400

    cipher program code added

--- cipher.py
+++ cipher.py
@@ -1,20 +1,20 @@
 # add your code here
 def caesar_cipher_encrypt(text, shift):
-    encrypted_text = ""
+    encrypted_sentence = ""
     for char in text:
         if char.isalpha():
             base = ord('a') if char.islower() else ord('A')
             encrypted_char = chr((ord(char) - base + shift) % 26 + base)
-            encrypted_text += encrypted_char
+            encrypted_sentence += encrypted_char
         else:
-            encrypted_text += char
-    return encrypted_text
+            encrypted_sentence += char
+    return encrypted_sentence
 
 def main():
     plain_text = input("Please enter a sentence: ")
     shift = 5
-    encrypted_text = caesar_cipher_encrypt(plain_text, shift)
-    print("The encryted sentence is:", encrypted_text)
+    encrypted_sentence = caesar_cipher_encrypt(plain_text, shift)
+    print("The encryted sentence is:", encrypted_sentence)
 
 if __name__ == "__main__":
     main()
